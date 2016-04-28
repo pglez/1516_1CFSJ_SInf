c6e441e33d85a4bf3729d64123f133553bbed64a
pglez <pglez@ieselcaminas.org>
2016-04-28 13:22:18 +0200

    directory sanity check, variables normalized

--- videobulk.sh
+++ videobulk.sh
@@ -2,20 +2,32 @@
 
 # Script to convert all video files from a directory (parameter) into mp4 format file
 
-# Sanity checks ?
+# Sanity checks
+# Directory provided as an argument?
+if [ $# -eq  0 ] 
+then 
+	Basedir=$PWD
+elif [ $# -eq 1 ] && [ -d $1 ]
+then  
+	Basedir=$1
+	[ ! "${Basedir: -1}" = "/" ] && Basedir=$Basedir"/"
+else
+	echo "Too many arguments or bad directory!"
+	echo "Usage: videobulk [directory]"
+fi
+# Other sanity checks... 
 # ...
 
 # Main code
 
-ls -1 $1*.avi > videofiles
-
-while read videofile
+ls -1 $Basedir*.avi > /tmp/.videofiles.tmp
+while read Videofile
 do
-	lengthname=${#videofile}
-	namefile=${videofile;0;$lengthname-4}
-	mencoder $1$videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
+	Lengthname=${#Videofile}
+	Namefile=${Videofile;0;$Lengthname-4}
+	mencoder $Basedir$Videofile -o $Namefile".mp4" -oac mp3lame -ovc lavc -of mp4
 
-done < videofiles
-rm videofiles
+done < /tmp/.videofiles.tmp
+rm /tmp/.videofiles.tmp
 
 exit 0
